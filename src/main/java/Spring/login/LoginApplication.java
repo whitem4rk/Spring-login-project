4ae89ace067a6a7313555f0da1b1748a9f0fb385
scheduled-for-hacker <whtmdgus56@nate.com>
2023-03-24 18:18:14 +0900

    chore: User entity can be used with jpa

--- src/main/java/Spring/login/LoginApplication.java
+++ src/main/java/Spring/login/LoginApplication.java
@@ -1,12 +1,34 @@
 package Spring.login;
 
+import Spring.login.domain.user.Grade;
+import Spring.login.domain.user.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"Spring.login.domain.user"})
 @SpringBootApplication
 public class LoginApplication {
 
 	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence");
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		User user = new User(null, "test", "testid", "testpw", Grade.CLIENT);
+		em.persist(user);
+		tx.commit();
+
+
+		em.close();
+		emf.close();
+
 		SpringApplication.run(LoginApplication.class, args);
 	}
 
--- src/main/java/Spring/login/domain/user/User.java
+++ src/main/java/Spring/login/domain/user/User.java
@@ -2,30 +2,34 @@ package Spring.login.domain.user;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "user")
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(length = 30, nullable = false, unique = true)
     @NotBlank(message = "유저명은 빈칸이거나 Null일 수 없습니다.")
     private String username;
+
     @Column(length = 30, nullable = false, unique = true)
     @NotBlank(message = "아이디는 빈칸이거나 Null일 수 없습니다.")
     private String userid;
+
     @Column(length = 30, nullable = false)
     @NotBlank(message = "패스워드는 빈칸이거나 Null일 수 없습니다.")
     private String password;
 
     @Column(length = 10, nullable = false)
-    @NotBlank(message = "등급은 빈칸이거나 Null일 수 없습니다.")
     @Enumerated(EnumType.STRING)
     private Grade grade;
 
@@ -37,3 +41,4 @@ public class User {
         this.username = username;
     }
 }
+
