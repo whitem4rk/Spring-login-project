a9b34c9d0fa3f208be340e6379c0a4f3c1425564
scheduled-for-hacker <whtmdgus56@nate.com>
2023-04-23 14:50:26 +0900

    feat: Created LoginRequest(DTO)

---
+++ src/main/java/Spring/domain/login/dto/LoginRequest.java
@@ -0,0 +1,21 @@
+package Spring.domain.login.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "아이디를 입력해주세요.")
+    @Length(min = 6, max = 30, message = "아이디은 6문자 이상 30문자 이하여야 합니다.")
+    private String userid;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Length(max = 20, message = "비밀번호는 20문자 이하여야 합니다.")
+    private String password;
+
+}
