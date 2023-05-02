e9a4952d61786bba600cd3ac55a66d9625bc8c4f
whitemark <whtmdgus56@nate.com>
2023-05-02 14:32:12 +0900

    feat: JwtDto

---
+++ src/main/java/Spring/domain/login/dto/JwtDto.java
@@ -0,0 +1,17 @@
+package Spring.domain.login.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class JwtDto {
+
+    private String type;
+
+    private String accessToken;
+
+    private String refreshToken;
+}
