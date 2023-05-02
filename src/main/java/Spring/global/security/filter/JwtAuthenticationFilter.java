2dbb8e80e82d7e27b4da67a4d366286dcf0f1df3
whitemark <whtmdgus56@nate.com>
2023-05-02 14:31:51 +0900

    feat: JwtAuthenticationFilter

---
+++ src/main/java/Spring/global/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,45 @@
+package Spring.global.security.filter;
+
+import Spring.global.token.JwtAuthenticationToken;
+import Spring.global.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(RequestMatcher matcher, JwtUtil jwtUtil) {
+        super(matcher);
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+
+        final String authorizationHeader = request.getHeader(AUTHORIZATION_HEADER);
+        final String jwt = jwtUtil.extractJwt(authorizationHeader);
+        final JwtAuthenticationToken authentication = JwtAuthenticationToken.of(jwt);
+
+        return super.getAuthenticationManager().authenticate(authentication);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+        Authentication authResult) throws IOException, ServletException {
+        SecurityContextHolder.getContext().setAuthentication(authResult);
+        chain.doFilter(request, response);
+    }
+}
