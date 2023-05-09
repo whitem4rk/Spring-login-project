214f918f25d8636668037208553f18605218e3de
whitemark <whtmdgus56@nate.com>
2023-05-09 23:05:05 +0900

    feat: JpaAuditingConfig

---
+++ src/main/java/Spring/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package Spring.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
