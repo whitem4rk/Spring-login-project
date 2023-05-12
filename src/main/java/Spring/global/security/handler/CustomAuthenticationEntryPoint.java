8177fe8ed753baa4cd515ddf324860b443c00b15
whitemark <whtmdgus56@nate.com>
2023-05-12 22:36:09 +0900

    feat: CustomAuthenticationEntryPoint

---
+++ src/main/java/Spring/global/security/handler/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package Spring.global.security.handler;
+
+import Spring.global.error.ErrorCode;
+import Spring.global.error.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Component
+@Slf4j
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+        response.setStatus(ErrorCode.JWT_INVALID.getStatus());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        try (OutputStream os = response.getOutputStream()) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(os, ErrorResponse.of(ErrorCode.JWT_INVALID));
+            os.flush();
+        }
+    }
+}
