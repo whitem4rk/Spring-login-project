8de22b9f75656e9ce60c1ce9bf944c1b5b1adc23
whitemark <whtmdgus56@nate.com>
2023-05-16 22:30:32 +0900

    test: Embedded redis test code

---
+++ src/test/java/Spring/domain/login/entity/redis/RefreshTokenTest.java
@@ -0,0 +1,40 @@
+package Spring.domain.login.entity.redis;
+
+import Spring.domain.login.repository.RefreshTokenRedisRepository;
+import Spring.global.config.EmbeddedRedisConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.context.annotation.Import;
+
+@Import(EmbeddedRedisConfig.class)
+@DataRedisTest
+class RefreshTokenTest {
+
+    @Autowired
+    private RefreshTokenRedisRepository refreshTokenRedisRepository;
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        refreshTokenRedisRepository.deleteAll();
+    }
+
+    @Test
+    public void enroll() {
+        //given
+        RefreshToken refreshToken = RefreshToken.builder()
+                .memberId(100L)
+                .value("100")
+                .build();
+        //when
+        refreshTokenRedisRepository.save(refreshToken);
+
+        //then
+        RefreshToken refreshToken1 = refreshTokenRedisRepository.findByMemberIdAndValue(100L, "100").get();
+        Assertions.assertThat(refreshToken1.getMemberId()).isEqualTo(100L);
+        Assertions.assertThat(refreshToken1.getValue()).isEqualTo("100");
+    }
+
+}
