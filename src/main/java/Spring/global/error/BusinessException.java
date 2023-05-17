f60c1fc9e08122dd213a20f3300ff31e48c62af2
whitemark <whtmdgus56@nate.com>
2023-05-17 17:38:35 +0900

    feat: Exception, ErrorResponse Util

--- src/main/java/Spring/global/error/BusinessException.java
+++ src/main/java/Spring/global/error/BusinessException.java
@@ -1,9 +1,16 @@
 package Spring.global.error;
 
 
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public class BusinessException extends RuntimeException {
 
     private ErrorCode errorCode;
+    private List<ErrorResponse.FieldError> errors = new ArrayList<>();
 
     public BusinessException(String message, ErrorCode errorCode) {
         super(message);
