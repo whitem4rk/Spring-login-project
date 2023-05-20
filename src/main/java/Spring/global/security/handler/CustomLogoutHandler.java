8fef50737c3245c72373c8acebc5cd50640e4def
whitemark <whtmdgus56@nate.com>
2023-05-20 22:30:21 +0900

    feat: LogoutHandler

---
+++ src/main/java/Spring/global/security/handler/CustomLogoutHandler.java
@@ -0,0 +1,30 @@
+package Spring.global.security.handler;
+
+import Spring.domain.login.exception.JwtInvalidException;
+import Spring.domain.login.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+
+@Component
+@RequiredArgsConstructor
+public class CustomLogoutHandler implements LogoutHandler {
+
+    private final UserService userService;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        final Cookie refreshToken = Arrays.stream(request.getCookies())
+                .filter(c -> c.getName().equals("refreshToken"))
+                .findFirst()
+                .orElseThrow(JwtInvalidException::new);
+
+        userService.logout(refreshToken.getValue());
+    }
+}
