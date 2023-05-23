763ddaa0523c8f4015b4d09479ac13fd1d5ac785
whitemark <whtmdgus56@nate.com>
2023-05-23 23:39:18 +0900

    feat: RegisterCode entity

---
+++ src/main/java/Spring/domain/login/entity/redis/RegisterCode.java
@@ -0,0 +1,34 @@
+package Spring.domain.login.entity.redis;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+
+@Getter
+@RedisHash
+public class RegisterCode implements Serializable {
+
+    @Id
+    @Indexed
+    private String userid;
+
+    private String email;
+
+    private String code;
+
+    @TimeToLive(unit = TimeUnit.SECONDS)
+    private Long timeout = 300L;
+
+    @Builder
+    public RegisterCode(String userid, String code, String email) {
+        this.userid = userid;
+        this.code = code;
+        this.email = email;
+    }
+}
