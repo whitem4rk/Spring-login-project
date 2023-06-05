f78341b121e7a0310a3314e43ea42fb579920999
whitemark <whtmdgus56@nate.com>
2023-06-05 23:58:32 +0900

    refactor: new JwtAuthenticationFilter

---
+++ src/main/java/Spring/global/security/filter/JwtAuthenticationFilter2.java
@@ -0,0 +1,50 @@
+package Spring.global.security.filter;
+
+import Spring.global.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter2 extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final static String ACCESS_TOKEN_SUBJECT = "accessToken";
+    private final static String REFRESH_TOKEN_SUBJECT = "refreshToken";
+
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        final Cookie[] cookies = request.getCookies();
+        final String accessJwt = jwtUtil.extractJwt(cookies, ACCESS_TOKEN_SUBJECT);
+
+        if (jwtUtil.expirationCheck(accessJwt)) {
+            Authentication authentication = jwtUtil.getAuthentication(accessJwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+            String refreshJwt = jwtUtil.extractJwt(cookies, REFRESH_TOKEN_SUBJECT);
+
+            if (jwtUtil.expirationCheck(refreshJwt)) {
+                Authentication authentication = jwtUtil.getAuthentication(refreshJwt);
+                final String newAccessJwt = jwtUtil.regenerateAccessJwt(authentication);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                response.addCookie(new Cookie(ACCESS_TOKEN_SUBJECT, newAccessJwt));
+            } else {
+                response.sendRedirect("/login");
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
